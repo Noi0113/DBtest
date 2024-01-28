032007057fbc3b171984b3eb2cb70b14ac387691
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-28 12:57:31 +0900

    Update DBtest.py

--- DBtest.py
+++ DBtest.py
@@ -21,16 +21,5 @@ def main():
     #ref = db.reference('restricted_access/secret_document')
     #print(ref.get())
 
-    count_down_sec = 5
-    for i in range(count_down_sec):
-        # プレースホルダーに残り秒数を書き込む
-        status_area.write(f'{count_down_sec - i} sec left')
-        # スリープ処理を入れる
-        time.sleep(1)
-
-    # 完了したときの表示
-    status_area.write('Done!')
-    # 風船飛ばす
-    st.balloons()
 if __name__ == '__main__':
     main()
