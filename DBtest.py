958d5f62566bd9470a8704c4b9d6b7a1fd363d76
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-28 11:46:07 +0900

    Update DBtest.py

--- DBtest.py
+++ DBtest.py
@@ -4,7 +4,18 @@ def main():
     status_area = st.empty()
 
 st.title('DBテストサイト')
-py -m pip install firebase-admin
+#py -m pip install firebase-admin
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+#cred = credentials.Certificate("path/to/serviceAccountKey.json")
+#firebase_admin.initialize_app(cred, {
+#    'databaseURL': 'https://<DATABASE_NAME>.firebaseio.com'
+#})
+
+#ref = db.reference('restricted_access/secret_document')
+#print(ref.get())
 
 if __name__ == '__main__':
     main()
