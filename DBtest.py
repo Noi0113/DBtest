635a6cda60e04ab20eb82b1c3a76b45ada904b76
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-28 13:11:50 +0900

    Update DBtest.py

--- DBtest.py
+++ DBtest.py
@@ -6,8 +6,6 @@ def main():
 
     st.title('DBテストサイト')
     st.markdown('firebase adminをインストールできた')
-
-
     import firebase_admin
     from firebase_admin import credentials
     from firebase_admin import db
