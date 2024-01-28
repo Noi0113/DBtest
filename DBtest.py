1d9aad14b60467d3544bd6ad6b27f93c0fcd874d
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-28 13:01:54 +0900

    Update DBtest.py

--- DBtest.py
+++ DBtest.py
@@ -8,9 +8,9 @@ def main():
     st.markdown('firebase adminをインストールできた')
 
 
-    #import firebase_admin
-    #from firebase_admin import credentials
-    #from firebase_admin import db
+    import firebase_admin
+    from firebase_admin import credentials
+    from firebase_admin import db
 
     #cred = credentials.Certificate("path/to/serviceAccountKey.json")
     #firebase_admin.initialize_app(cred, {
