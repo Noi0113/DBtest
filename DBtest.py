538e1f20f00059ab86915b6eb90c10052155de73
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-28 12:13:09 +0900

    Update DBtest.py

--- DBtest.py
+++ DBtest.py
@@ -1,37 +1,22 @@
 import streamlit as st
-
+#py get-pip.py
+!pip install firebase-admin
 def main():
     status_area = st.empty()
 
-st.title('DBテストサイト')
-#py get-pip.py
-#pip install firebase-admin
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
+    st.title('DBテストサイト')
 
-#cred = credentials.Certificate("path/to/serviceAccountKey.json")
-#firebase_admin.initialize_app(cred, {
-#    'databaseURL': 'https://<DATABASE_NAME>.firebaseio.com'
-#})
+    import firebase_admin
+    from firebase_admin import credentials
+    from firebase_admin import db
 
-#ref = db.reference('restricted_access/secret_document')
-#print(ref.get())
+    #cred = credentials.Certificate("path/to/serviceAccountKey.json")
+    #firebase_admin.initialize_app(cred, {
+    #    'databaseURL': 'https://<DATABASE_NAME>.firebaseio.com'
+    #})
 
+    #ref = db.reference('restricted_access/secret_document')
+    #print(ref.get())
 
-import time
-
-    # カウントダウン
-    count_down_sec = 5
-    for i in range(count_down_sec):
-        # プレースホルダーに残り秒数を書き込む
-        status_area.write(f'{count_down_sec - i} sec left')
-        # スリープ処理を入れる
-        time.sleep(1)
-
-    # 完了したときの表示
-    status_area.write('Done!')
-    # 風船飛ばす
-    st.balloons()
 if __name__ == '__main__':
     main()
