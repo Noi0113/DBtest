e5ca5279e7d83c98cd5bffc4ff7de2bd3efd22bd
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-28 11:19:21 +0900

    Update DBtest.py

--- DBtest.py
+++ DBtest.py
@@ -4,8 +4,7 @@ def main():
     status_area = st.empty()
 
 st.title('DBテストサイト')
-python -m pip install --upgrade pip
-pip install firebase-admin
+py -m pip install firebase-admin
 
 if __name__ == '__main__':
     main()
