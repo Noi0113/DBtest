64bf44461b5fa887143f7fd37dffd9fb866ec3ac
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-28 13:26:37 +0900

    Update DBtest.py

--- DBtest.py
+++ DBtest.py
@@ -1,15 +1,17 @@
 #py get-pip.py
 #!pip install firebase-admin
 
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
+
 import streamlit as st
 def main():
     status_area = st.empty()
 
     st.title('DBテストサイト')
-    #st.markdown('firebase adminをインストールできた')
+    st.markdown('firebase adminをインストールできた')
+
+    import firebase_admin
+    from firebase_admin import credentials
+    from firebase_admin import db
     #cred = credentials.Certificate("path/to/serviceAccountKey.json")
     #firebase_admin.initialize_app(cred, {
     #    'databaseURL': 'https://<DATABASE_NAME>.firebaseio.com'
