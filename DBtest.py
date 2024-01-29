a88312365e383907337056558fed7434fa73a272
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-30 05:06:26 +0900

    Add files via upload

---
+++ DBtest.py
@@ -0,0 +1,23 @@
+import streamlit as st
+def main():
+    status_area = st.empty()
+
+    st.title('DBテストサイト')
+    st.markdown('requirements.txtをアップロードした')
+    import pulp
+    #py get-pip.py
+    #!pip install firebase-admin
+    #import pandas as pd
+    #import firebase_admin
+    #from firebase_admin import credentials
+    #from firebase_admin import db
+    #cred = credentials.Certificate("path/to/serviceAccountKey.json")
+    #firebase_admin.initialize_app(cred, {
+    #    'databaseURL': 'https://<DATABASE_NAME>.firebaseio.com'
+    #})
+
+    #ref = db.reference('restricted_access/secret_document')
+    #print(ref.get())
+
+if __name__ == '__main__':
+    main()
