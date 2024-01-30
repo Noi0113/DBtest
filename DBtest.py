4cd8ed6b27dca0fd5a2282b7fd24bfa276ebe0c7
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-30 10:20:54 +0900

    Update DBtest.py

--- DBtest.py
+++ DBtest.py
@@ -4,18 +4,18 @@ def main():
 
     st.title('DBテストサイト')
     st.markdown('requirements.txtをアップロードした。各モジュールは使えるか？')
-    #import pyrebase4
+    import pyrebase4
     import pandas as pd
     import firebase_admin
     from firebase_admin import credentials
     from firebase_admin import db
-    cred = credentials.Certificate("path/to/serviceAccountKey.json")
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://console.firebase.google.com/u/0/project/monketsu-choicedb/database/monketsu-choicedb-default-rtdb/data?hl=ja'
-    })
+    #cred = credentials.Certificate("path/to/serviceAccountKey.json")
+    #firebase_admin.initialize_app(cred, {
+    #    'databaseURL': 'https://console.firebase.google.com/u/0/project/monketsu-choicedb/database/monketsu-choicedb-default-rtdb/data?hl=ja'
+    #})
 
-    ref = db.reference('restricted_access/secret_document')
-    print(ref.get())
+    #ref = db.reference('restricted_access/secret_document')
+    #print(ref.get())
 
 if __name__ == '__main__':
     main()
