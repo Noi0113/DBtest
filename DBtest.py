d36fa56bee86a9929ca407778089eb678161bf43
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-30 10:30:58 +0900

    Update DBtest.py

--- DBtest.py
+++ DBtest.py
@@ -4,7 +4,7 @@ def main():
 
     st.title('DBテストサイト')
     st.markdown('requirements.txtをアップロードし編集した。仮想環境でもインストール済み')
-    import pyrebase4
+    import Pyrebase4
     import pandas as pd
     import firebase_admin
     from firebase_admin import credentials
