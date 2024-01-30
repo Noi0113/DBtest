16923c0db03ea3942c8ee9d2bd56f4d7be4220d9
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-30 20:38:28 +0900

    Create アンケート回答ページ.py

---
+++ アンケート回答ページ.py
@@ -0,0 +1,26 @@
+import streamlit as st
+def main():
+    status_area = st.empty()
+#タイトル
+st.title('アンケート回答') 
+
+st.markdown('大会ID、氏名、大学名など入力する欄がこの辺に来る')
+
+#ボタン
+st.button('回答を送信',use_container_width=True,help='ページ準備中')
+st.markdown('回答が送信されたら「完了しました」的な何か出力させたい。ページも変えられたら〇')
+
+
+##ログインについて
+#st.link_button()を導入したい
+
+
+#######トップページ終わり
+
+#######新規作成クリック後のページ
+def new():
+    st.sidebar.title("ページが切り替わりました")
+    st.markdown("## 次のページです")
+
+if __name__ == '__main__':
+    main()
