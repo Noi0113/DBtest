072d5284cead76b695b3769d1954659d32b97c3e
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-30 10:32:42 +0900

    Update DBtest.py

--- DBtest.py
+++ DBtest.py
@@ -3,7 +3,7 @@ def main():
     status_area = st.empty()
 
     st.title('DBテストサイト')
-    st.markdown('requirements.txtをアップロードし編集した。仮想環境でもインストール済み')
+    st.markdown('requirements.txtをアップロードし編集した。誤字チェック済み')
     import Pyrebase4
     import pandas as pd
     import firebase_admin
