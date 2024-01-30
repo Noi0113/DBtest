7c23f2a626e5e1c69fb008585eb08dc6033f6658
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-30 22:35:06 +0900

    Update アンケート回答ページ.py

--- アンケート回答ページ.py
+++ アンケート回答ページ.py
@@ -11,6 +11,18 @@ st.button('回答を送信',use_container_width=True,help='ページ準備中')
 st.markdown('回答が送信されたら「完了しました」的な何か出力させたい。ページも変えられたら〇')
 
 
+# フォームを作成します
+with st.form(key='my_form'):
+    input_name = st.text_input(label='名前')
+    input_age = st.number_input(label='年齢', min_value=0)
+    input_gender = st.selectbox('性別', options=['男性', '女性', 'その他'])
+    input_feedback = st.text_area(label='フィードバック')
+    submit_button = st.form_submit_button(label='送信')
+
+# ユーザーが送信ボタンを押したときに表示されるメッセージ
+if submit_button:
+    st.write(f"ありがとうございます、{input_name}さん！")
+
 ##ログインについて
 #st.link_button()を導入したい
 
