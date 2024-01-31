ee859ab7e9a160879a02d79344f427c9c0260ae3
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-31 14:44:58 +0900

    Update 新規作成ページ.py

--- 新規作成ページ.py
+++ 新規作成ページ.py
@@ -40,44 +40,23 @@ def main():
     status_area = st.empty()
 #タイトル
 st.title('新規作成') 
-
-st.markdown('大会名・大学名など入力する欄がこの辺に来る')
+st.markdown('新規大会IDとパスワードの作成をする')
+#st.markdown('大会名・大学名など入力する欄がこの辺に来る')
 
 #ボタン
 #st.button('ID発行',use_container_width=True,help='ページ準備中')
 #st.markdown('ID発行されたらそのIDと「完了しました」的な何か出力させたい。ページも変えられたら〇')
 
-
-# ページの選択と表示
-menu = ["大会ログイン", "新規大会登録" ]
-choice = st.selectbox("選択してください",menu)
-st.write("{}ページです".format(choice))
-
-if choice == "大会ログイン":
-	username = st.text_input("大会名を入力")
-	password = st.text_input("大会パスワードを入力してください",type='password')
-	if st.button('ログイン'):
-		hashed_pswd = make_hashes(password)
-		result = login_user(username,check_hashes(password,hashed_pswd))
-		if result:
-			st.success("大会名：{}でログインしました".format(username))
-		else:
-			st.warning("大会名か大会パスワードが間違っています")
-
-elif choice == "新規大会登録":
-	new_user = st.text_input("大会名を入力してください（被りがあると注意されて新規作成できない予定）")
-	new_password = st.text_input("大会パスワードを入力してください",type='password')
-	if st.button("登録"):
-		if add_user(new_user,make_hashes(new_password)):
-			st.warning("その大会名は既にしようされています")
-		else:
-			create_user()
-			st.success("新しい大会の作成に成功しました")
-			st.info("大会ログイン画面からログインしてください")
-
-
-
-
+#ここから本作成
+new_user = st.text_input("大会名を入力してください（被りがあると注意されて新規作成できない予定）")
+new_password = st.text_input("大会パスワードを入力してください",type='password')
+if st.button("登録"):
+	if add_user(new_user,make_hashes(new_password)):
+		st.warning("その大会名は既にしようされています")
+	else:
+		create_user()
+		st.success("新しい大会の作成に成功しました")
+		st.info("大会ログイン画面からログインしてください")
 
 ##ログインについて
 #st.link_button()を導入したい
