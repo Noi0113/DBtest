d23d141e051fc07a1eedf69d6f82e5cdb38dd8a9
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-31 14:49:18 +0900

    Update 新規作成ページ.py

--- 新規作成ページ.py
+++ 新規作成ページ.py
@@ -41,16 +41,13 @@ def main():
 #タイトル
 st.title('新規作成') 
 st.markdown('新規大会IDとパスワードの作成をする')
-st.markdown('大会名・大学名など入力する欄がこの辺に来る')
-
-#ボタン
-st.button('ID発行',use_container_width=True,help='ページ準備中')
+#st.markdown('大会名・大学名など入力する欄がこの辺に来る')
 st.markdown('ID発行されたらそのIDと「完了しました」的な何か出力させたい。ページも変えられたら〇')
 
 #ここから本作成
 new_user = st.text_input("大会名を入力してください（被りがあると注意されて新規作成できない予定）")
 new_password = st.text_input("大会パスワードを入力してください",type='password')
-if st.button("登録"):
+if st.button('ID発行',use_container_width=True,help='ページ準備中'):
 	if add_user(new_user,make_hashes(new_password)):
 		st.warning("その大会名は既にしようされています")
 	else:
