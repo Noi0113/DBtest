8b8548885f098656d3424b43e9649fcfdd752317
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-31 15:33:55 +0900

    Update 新規作成ページ.py

--- 新規作成ページ.py
+++ 新規作成ページ.py
@@ -12,8 +12,6 @@ st.markdown('新規大会IDとパスワードの作成をする')
 #st.markdown('大会名・大学名など入力する欄がこの辺に来る')
 st.markdown('ID発行されたらそのIDと「完了しました」的な何か出力させたい。ページも変えられたら〇')
 
-#機能の追加
-#ここからログイン機能について必要な定義
 #sqliteに接続
 conn = sqlite3.connect('user_database.db') #ここのデータベース名を「monketsu-option.db」に変更？
 c=conn.cursor()
@@ -49,7 +47,7 @@ def check_hashes(password,hashed_text):
 #ここから本作成
 new_user = st.text_input("大会名を入力してください（被りがあると注意されて新規作成できない予定）")
 new_password = st.text_input("大会パスワードを入力してください",type='password')
-new_check = st.markdown('大会名とパスワードを記録しておいてください')
+#new_check = st.markdown('大会名とパスワードを記録しておいてください')
 new_taikai = st.button('ID発行',use_container_width=True,help='ページ準備中')
 if new_taikai:
 	if add_user(new_user,make_hashes(new_password)):
