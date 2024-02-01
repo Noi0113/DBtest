a60e45cbb8b4acc93cce01ef54bf55bef314f6f4
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-02 06:12:42 +0900

    Update アンケート回答ページ.py

--- アンケート回答ページ.py
+++ アンケート回答ページ.py
@@ -3,26 +3,32 @@ import pandas as pd
 import sqlite3
 import hashlib
 
+def get_connection():
+    if 'conn' not in st.session_state:
+        st.session_state['conn'] = sqlite3.connect('monketsu.db')
+    return st.session_state['conn']
+
 #target_id列の値がtarget_idである行のcolumn_name列の値をリストで出す
-def data_retu(database_path, table_name, target_name,target_id, column_name):
-    conn = sqlite3.connect(database_path)
-    cursor = conn.cursor()
+def data_retu(table_name, target_name,target_id, column_name):
+    conn = get_connection()
+    c = conn.cursor()
     query = f"SELECT {column_name} FROM {table_name} WHERE {target_name} = ?;"
-    cursor.execute(query, (target_id,))
-    result = cursor.fetchall()
+    c.execute(query, (target_id,))
+    result = c.fetchall()
     conn.close()
     result_list = [item[0] for item in result]
     return result_list
 
 #loginする
 def login_user(id,pas):
+    conn = get_connection()
+    c = conn.cursor()
     c.execute('SELECT * FROM taikai_data WHERE taikaiid =? AND password = ?',(id,pas))
     data = c.fetchall()
     return data
 #hash化
 def make_hashes(password):
     return hashlib.sha256(str.encode(password)).hexdigest()
-
 def check_hashes(password,hashed_text):
     if make_hashes(password) == hashed_text:
         return hashed_text
@@ -83,9 +89,11 @@ def main():
                     absent_01.append(1)
             while len(absent_01) < 15:
                 absent_01.append(0)
-    
+
+            conn = get_connection()
+            c = conn.cursor()
             c.execute('''
-                INSERT INTO user_data (name,school,level,kisuu,wantto,wantnotto,s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15) VALUES ((?,?,?,?,?,?,?,?,?,?,?,?,,?,?,?,?,?,?,?,?)
+                INSERT INTO user_data (name,school,level,kisuu,wantto,wantnotto,s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15) VALUES ((?,?,?,?,?,?,?,?,?,?,?,?,,?,?,?,?,?,?,?,?);
                 ''', (input_name,input_school,input_level,input_kisuu,input_wantto,input_wantnotto,absent_01[0],absent_01[1],absent_01[2],absent_01[3],absent_01[4],absent_01[5],absent_01[6],absent_01[7],absent_01[8],absent_01[9],absent_01[10],absent_01[11],absent_01[12],absent_01[13],absent_01[14]))
             conn.commit()
             st.success(f"送信が完了しました。ありがとうございます、{input_name}さん！")
