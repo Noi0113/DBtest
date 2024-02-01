e0fbd97d5aaa2d436b263c710dd9c697990e7b06
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-02 02:33:08 +0900

    Update 新規作成ページ.py

--- 新規作成ページ.py
+++ 新規作成ページ.py
@@ -11,17 +11,22 @@ def get_connection():
 #def create_taikai():
 #	c.execute('CREATE TABLE IF NOT EXISTS taikai_table(username TEXT,password TEXT)')
 
-def add_taikai(username, password,num):
-    # 大会IDが既に存在するかを確認
-    conn = get_connection() #ここでコネクション確立？？name errorが出てしまう
-    c.execute('SELECT * FROM taikai_data WHERE taikaiid = ?', (username,))
-    existing_user = c.fetchone()
-    if existing_user:
-        return True
-    else:
-        c.execute('INSERT INTO taikai_data(taikaiid, password,snum) VALUES (?, ?, ?)', (username, password,num))
-        conn.commit()
-        return False
+def is_taikaiid_exists(taikaiid):
+    # データベースに接続
+    conn = get_connection()
+    c = conn.cursor()
+    c.execute(f"SELECT COUNT(*) FROM taikai_data WHERE taikaiid = ?;", (taikaiid,))
+    count = c.fetchone()[0]
+    conn.close()
+    return count > 0
+
+def add_new_data(taikaiid, password):
+    conn =  get_connection()
+    c = conn.cursor()
+    c.execute("INSERT INTO taikai_data (taikaiid, password,num_match) VALUES (?, ?);", (taikaiid, password,num_match))
+    conn.commit()
+    # データベース接続を閉じる
+    conn.close()
 
 # パスワードのハッシュ化
 def make_hashes(password):
@@ -36,7 +41,7 @@ def main():
     #st.markdown('ID発行されたらそのIDと「完了しました」的な何か出力させたい。ページも変えられたら〇')
 
     # ここから本作成
-    new_taikai = st.text_input("大会名を入力してください（被りがあると注意されて新規作成できない予定）")
+    new_taikaiid = st.text_input("大会名を入力してください（被りがあると注意されて新規作成できない予定）")
     new_password = st.text_input("大会パスワードを入力してください",type='password')
     num_match = st.selectbox("大会の試合数を入力してください", range(1, 15),format_func=lambda x: f'{x} 回')
     num_universities = st.number_input("参加大学数を入力してください", min_value=1, step=1)
@@ -47,13 +52,19 @@ def main():
         universities.append(university_name)
 
     if st.button('大会作成！', use_container_width=True, help='ページ準備中'):
-        if add_user(new_taikai, make_hashes(new_password),num_match):
-            st.warning("その大会名は既に使用されています")
+        if is_taikaiid_exists(new_taikaiid):
+            st.error("エラー: このtaikaiidは既に存在します。別のtaikaiidを入力してください。")
         else:
-            create_user()
+            add_new_data(new_taikaiid, new_password,num_match)
             st.success(f"新しい大会({new_taikai})の作成に成功しました")
-            st.info("参加者にアンケートのURL（https://monketsu-questionnaire.streamlit.app/）を送ってください。")
-            st.info("アンケートの回答には大会IDと大会パスワードの入力が必要です")
+            
+        #if add_user(new_taikai, make_hashes(new_password),num_match):
+        #    st.warning("その大会名は既に使用されています")
+        #else:
+        #    create_user()
+        #    st.success(f"新しい大会({new_taikai})の作成に成功しました")
+        #    st.info("参加者にアンケートのURL（https://monketsu-questionnaire.streamlit.app/）を送ってください。")
+        #    st.info("アンケートの回答には大会IDと大会パスワードの入力が必要です")
             
 
 if __name__ == '__main__':
