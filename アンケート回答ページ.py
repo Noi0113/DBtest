87a1bcb7e4e81ee07d9925832108bdc61b9b0d91
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-01 10:18:06 +0900

    Update アンケート回答ページ.py

--- アンケート回答ページ.py
+++ アンケート回答ページ.py
@@ -8,14 +8,7 @@ st.title('アンケート回答')
 st.markdown('フォーム')
 
 
-# オプションのリスト
-options = ['Option 1', 'Option 2', 'Option 3', 'Option 4']
 
-# マルチセレクトウィジェット
-selected_options = st.multiselect('Select your options', options)
-
-# 選択されたオプションを表示
-st.write('You selected:', ', '.join(selected_options))
 univ_options = ['あ','い']#こんな感じで、データベースから大学名のリストを取ってくればプルダウン作成は可能です！！！
 
 # フォームを作成します
@@ -25,6 +18,16 @@ with st.form(key='my_form'):
     input_level = st.selectbox('級',options=['A','B','C','D','E'])
     input_univ = st.selectbox('大学名', options=univ_options)
     #input_feedback = st.text_area(label='フィードバック')
+    options = ['Option 1', 'Option 2', 'Option 3', 'Option 4']
+    absent_matchs = st.multiselect('Select your options', options)
+
+    # オプションのリスト
+    options = ['Option 1', 'Option 2', 'Option 3', 'Option 4']　#このリストをmonketsu-option.dbから取得するとよい
+    # マルチセレクトウィジェット
+    selected_options = st.multiselect('Select your options', options)
+    # 選択されたオプションを表示
+    st.write('欠席する試合:', ', '.join(selected_options))
+    
     st.markdown ('個人IDを作成してください。アンケート結果を編集する際に必要となりますので、お手元にお控え下さい。')
     input_kojinid = st.text_input(label = '個人ID')
 
