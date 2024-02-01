f80125a322747b54d559fdfd18ec90c3fc419450
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-02 05:33:22 +0900

    Update 新規作成ページ.py

--- 新規作成ページ.py
+++ 新規作成ページ.py
@@ -50,6 +50,7 @@ def main():
         universities.append(university_name)
 
     if st.button('大会作成！', use_container_width=True, help='ページ準備中'):
+        st.write(type(num_match))
         if is_taikaiid_exists(new_taikaiid):
             st.error("エラー: このtaikaiidは既に存在します。別のtaikaiidを入力してください。")
         else:
