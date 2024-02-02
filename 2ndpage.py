d2ca312e97ae7b5fd83b45ebb30c68c75b826f82
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-02 15:35:07 +0900

    Update 2ndpage.py

--- 2ndpage.py
+++ 2ndpage.py
@@ -92,10 +92,12 @@ def main():
         
         
         #所属の集合
-        #S = [row.所属 for row in df.itertuples()]
-        #S = list(set(S))     ##ここ2回目以降は再起動しないとエラーになる
+        S = []
+        S = [row.所属 for row in df.itertuples()]
+        S = list(set(S))     ##ここ2回目以降は再起動しないとエラーになる
         
         #級の集合
+        K = []
         K = [row.級 for row in df.itertuples()]
         K = list(set(K))
         
