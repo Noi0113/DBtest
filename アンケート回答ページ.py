877dc13b19e1c0058ca19310dcd40b315df603ae
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-02 15:01:29 +0900

    Update アンケート回答ページ.py

--- アンケート回答ページ.py
+++ アンケート回答ページ.py
@@ -29,7 +29,7 @@ def make_hashes(password):
     return hashlib.sha256(str.encode(password)).hexdigest()
 def check_hashes(password,hashed_text):
     if make_hashes(password) == hashed_text:
-        return hashed_text
+        return True
     return False
     
 def main():
