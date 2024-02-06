4b051cc7519e9946bf32af01b2b1324b681d20fc
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-06 14:01:51 +0900

    Update 2ndpage.py

--- 2ndpage.py
+++ 2ndpage.py
@@ -54,10 +54,6 @@ def check_hashes(password,hashed_text):
 def main():
     status_area = st.empty()
     #ここから上は編集しない
-
-    #メモリ開放
-    del df,Q,S,K,I_all,Iall_old,I_rest,I_sanka_old,I_sanka,I_d,P,I_wanttorest,Wantto,Dontwantto
-    del S_dict,K_dict
     
     #タイトル
     st.title('対戦表の作成')
