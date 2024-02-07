eebddfea027ed212bc0aca16a54fa0ae5e9012f8
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-08 01:44:06 +0900

    Create ログインページ(対戦表確認前).py

---
+++ ログインページ(対戦表確認前).py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('ログイン機能(対戦表確認前)')
+st.markdown('この辺にログイン機能(適当に対戦表確認ページに飛べるボタンを設置しておきました)')
+
+st.link_button('対戦表の確認',"https://monketsu-2ndpage.streamlit.app/",use_container_width=True)
