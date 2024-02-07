f6bb3b4c41d072ff34884a2afc37d7d26d73b278
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-08 01:42:49 +0900

    Create ログインページ(アンケ前).py

---
+++ ログインページ(アンケ前).py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('ログイン機能(アンケ前)')
+st.markdown('この辺にログイン機能(適当にアンケート回答ページに飛べるボタンを設置しておきました)')
+
+st.link_button('アンケート回答',"https://monketsu-questionnaire.streamlit.app/",use_container_width=True)
