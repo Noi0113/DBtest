2e89eb17ee7d5c42114c28ca4b450a50e08ae95a
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-08 09:46:59 +0900

    Update 2ndpage.py

--- 2ndpage.py
+++ 2ndpage.py
@@ -71,9 +71,9 @@ def main():
 #↓以降最適化の実行
 
         
-        uploaded_file = st.file_uploader("CSVファイルを選択してください。(CSVファイルを読み込み表示させられます。今後最適化を実験するときのために使えるかも)", type="csv")
-        if uploaded_file is not None:
-            df = pd.read_csv(uploaded_file)
+    uploaded_file = st.file_uploader("CSVファイルを選択してください。(CSVファイルを読み込み表示させられます。今後最適化を実験するときのために使えるかも)", type="csv")
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
         #df = pd.DataFrame()
         #conn = sqlite3.connect('monka.db')
         #c = conn.cursor()
