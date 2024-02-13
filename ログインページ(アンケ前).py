f6038cc398e469c228fc8f211ddc0256729de773
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-13 21:22:08 +0900

    Update ログインページ(アンケ前).py

--- ログインページ(アンケ前).py
+++ ログインページ(アンケ前).py
@@ -2,14 +2,12 @@ import streamlit as st
 import hashlib
 import sqlite3
 
-st.title('ログインページ!(アンケ前)')
 #loginする
-def login_user(id,pas):
-    conn = sqlite3.connect('monka.db')
-    c = conn.cursor()
-    c.execute('SELECT * FROM taikai_data WHERE taikaiid =? AND password = ?',(id,pas))
-    data = c.fetchall()
-    conn.close()
+def login_user(username,password):
+    c = sqlite3.connect('monka.db')
+    cursor = c.cursor()
+    cursor.execute('SELECT * FROM taikai_data WHERE taikaiid = ? AND password = ?', (username, password))
+    data = cursor.fetchall()
     return data
 #hash化
 def make_hashes(password):
@@ -22,18 +20,21 @@ def check_hashes(password,hashed_text):
 def main():
   status_area = st.empty()
   st.title('ログイン機能(アンケ前)')
-  #st.markdown('この辺にログイン機能(適当にアンケート回答ページに飛べるボタンを設置しておきました)')
   st.markdown('参加する大会の大会名とパスワードを入力してください')
 
   input_taikaiid = st.text_input(label = '大会名を入力してください')
   input_password = st.text_input(label = "大会パスワードを入力してください",type='password')
 
   if st.button(label='確定'):
-      hashed_pswd = make_hashes(input_password)
-      result = login_user(input_taikaiid,check_hashes(input_password,hashed_pswd))
+      #hash化されたpasswordをdbに書き込めるようになったらこれ
+      #hashed_pswd = make_hashes(input_password)
+      #result = login_user(input_taikaiid,check_hashes(input_password,hashed_pswd))
+      
+      result = login_user(input_taikaiid,input_password)
+      
       if result:
-          st.write("ログインしました")
-        #st.success("{}の参加用フォーム".format(input_taikaiid))
+        st.write("ログインしました")
+        st.success("{}の参加用フォーム".format(input_taikaiid))
 
         
 #st.link_button('アンケート回答',"https://monketsu-questionnaire.streamlit.app/",use_container_width=True)
