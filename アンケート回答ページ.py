032fdd6fd88480c31f253fd8a1bea07b6a2a02ce
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-15 00:19:14 +0900

    Update アンケート回答ページ.py

--- アンケート回答ページ.py
+++ アンケート回答ページ.py
@@ -8,6 +8,33 @@ import hashlib
 #        st.session_state['conn'] = sqlite3.connect('monketsu.db')
 #    return st.session_state['conn']
 
+def form_open():
+                if input_name and input_univ and input_level:
+                    absent_01 = []
+                    for i in absent_options:
+                        if i in absent_matches:
+                            absent_01.append(0)
+                        else:
+                            absent_01.append(1)
+                    while len(absent_01) < 16:
+                        absent_01.append(0)
+
+                    conn = conn.sqlite3.connect('monketsu.db')
+                    c = conn.cursor()
+                    c.execute('''
+                        INSERT INTO user_data (name, school, level, kisuu, wantto, wantnotto, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, taikaiid)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+                        ''', (input_name, input_univ, input_level, input_kisuu, input_wantto, input_wantnotto,absent_01[0], absent_01[1], absent_01[2], absent_01[3], absent_01[4], absent_01[5],absent_01[6], absent_01[7], absent_01[8], absent_01[9], absent_01[10], absent_01[11],absent_01[12], absent_01[13], absent_01[14], input_taikaiid))
+
+                    conn.commit()
+                    conn.close()
+                    st.success(f"送信が完了しました。ありがとうございます、{input_name}さん！")
+                # 全ての欄が埋まっていない場合の処理
+                else:
+                    st.warning("必須項目を入力してください。")
+
+
+
 #target_id列の値がtarget_idである行のcolumn_name列の値をリストで出す
 def data_retu(table_name, target_name,target_id, column_name):
     conn = sqlite3.connect('monka.db')
@@ -64,42 +91,23 @@ def main():
             absent_options.append(f'{i+1}試合目')
 
             # フォームを作成します
-        #with st.form(key='my_form'):
-        input_name = st.text_input(label='名前を入力してください(必須)')
-        input_univ = st.selectbox('学校名または所属会名を入力してください(必須)', options=univ_options)
-        input_level = st.selectbox('級を入力してください(必須)',options=['A','B','C','D','E'])
-        input_kisuu = st.selectbox('奇数の場合一人取りまたは読手を希望しますか(必ず希望に添えるわけではありません)',options=['はい','いいえ'])
-        input_wantto = st.text_input(label='対戦したい人を記入してください')
-        input_wantnotto = st.text_input(label='対戦したくない人を記入してください')
-        absent_matches = st.multiselect('欠席する試合を入力してください(複数選択可)', absent_options)
+        with st.form(key='my_form'):
+            input_name = st.text_input(label='名前を入力してください(必須)')
+            input_univ = st.selectbox('学校名または所属会名を入力してください(必須)', options=univ_options)
+            input_level = st.selectbox('級を入力してください(必須)',options=['A','B','C','D','E'])
+            input_kisuu = st.selectbox('奇数の場合一人取りまたは読手を希望しますか(必ず希望に添えるわけではありません)',options=['はい','いいえ'])
+            input_wantto = st.text_input(label='対戦したい人を記入してください')
+            input_wantnotto = st.text_input(label='対戦したくない人を記入してください')
+            absent_matches = st.multiselect('欠席する試合を入力してください(複数選択可)', absent_options)
     
-            #submit_button = st.form_submit_button(label='送信',use_container_width = True)
-
+            submit_button = st.form_submit_button(label='送信',use_container_width = True)
+            if get_data_source:
+                # 取得したデータは session_state オブジェクトに保存します
+                st.session_state.hoge = 3
+                st.text("データ取得しました。")
                 # ユーザーが送信ボタンを押したときに表示されるメッセージ
-        if st.button(label='送信'):
-            if input_name and input_univ and input_level:
-                    absent_01 = []
-                    for i in absent_options:
-                        if i in absent_matches:
-                            absent_01.append(0)
-                        else:
-                            absent_01.append(1)
-                    while len(absent_01) < 16:
-                        absent_01.append(0)
-
-                    conn = conn.sqlite3.connect('monketsu.db')
-                    c = conn.cursor()
-                    c.execute('''
-                        INSERT INTO user_data (name, school, level, kisuu, wantto, wantnotto, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, taikaiid)
-                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
-                        ''', (input_name, input_univ, input_level, input_kisuu, input_wantto, input_wantnotto,absent_01[0], absent_01[1], absent_01[2], absent_01[3], absent_01[4], absent_01[5],absent_01[6], absent_01[7], absent_01[8], absent_01[9], absent_01[10], absent_01[11],absent_01[12], absent_01[13], absent_01[14], input_taikaiid))
-
-                    conn.commit()
-                    conn.close()
-                    st.success(f"送信が完了しました。ありがとうございます、{input_name}さん！")
-                # 全ての欄が埋まっていない場合の処理
-            else:
-                    st.warning("必須項目を入力してください。")
+            if submit_button:
+                form_open()
       else:
         st.warning("大会名か大会パスワードが間違っています")
 
