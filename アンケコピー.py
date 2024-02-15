7db84d60a8097bf702122ef8b83444bd2225ab46
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-16 07:42:36 +0900

    Update アンケコピー.py

--- アンケコピー.py
+++ アンケコピー.py
@@ -12,12 +12,12 @@ gc = gspread.authorize(credentials)
 
 def main():
     # 以下は、未だ入力がされていない時に-を表示できるようにした、ということ？？
-    if 'univ_options' not in st.session_state: 
-        st.session_state.univ_options = ["-"]
-    if 's_number' not in st.session_state: 
-        st.session_state.s_number = []
-    if 'absent_options' not in st.session_state: 
-        st.session_state.absent_options = ["-"]
+    #if 'univ_options' not in st.session_state: 
+     #   st.session_state.univ_options = ["-"]
+    #if 's_number' not in st.session_state: 
+     #   st.session_state.s_number = []
+    #if 'absent_options' not in st.session_state: 
+     #   st.session_state.absent_options = ["-"]
         
     # まずGoogle Sheetsのシート2を開き、それをデータフレーム化する
     new_gene_sheet = gc.open('monketsu-karuta-db').get_worksheet(1)
@@ -26,7 +26,7 @@ def main():
     new_gene_df = pd.DataFrame(new_gene_data, columns=headers)
     
     # タイトル
-    st.title('アンケート回答ページ！') 
+    st.title('アンケート回答ページコピー') 
     st.markdown('参加する大会の大会名とパスワードを入力してください')
     input_taikaiid = st.text_input(label='大会名を入力してください')
     input_password = st.text_input(label="大会パスワードを入力してください", type='password')
@@ -64,35 +64,26 @@ def main():
                 absent_matches = st.multiselect('欠席する試合を入力してください(複数選択可)', options=absent_options)
                 
                 submit_button = st.form_submit_button(label='送信', use_container_width=True)
-            
-            # ユーザーが送信ボタンを押したときに表示されるメッセージ
-            if submit_button:
-                if input_name and input_univ and input_level:
-                    # 欠席情報をリストで表現
-                    absent_bin_list = [1 if option in absent_matches else 0 for option in absent_options]
-                    while len(absent_bin_list) < 16:
-                        absent_bin_list.append(0)
-                    
-                    # スプレッドシートへの書き込み
-                    try:
-                        sheet = gc.open('monketsu-karuta-db').get_worksheet(0)
-                        last_row = len(sheet.col_values(3)) + 1  # 空欄を許すための処置
-                        sheet.update_cell(last_row, 1, input_taikaiid)
-                        sheet.update_cell(last_row, 2, input_password)
-                        sheet.update_cell(last_row, 3, input_name)
-                        sheet.update_cell(last_row, 4, input_univ)
-                        sheet.update_cell(last_row, 5, input_level)
-                        sheet.update_cell(last_row, 6, input_kisuu)
-                        sheet.update_cell(last_row, 7, input_wantto)
-                        sheet.update_cell(last_row, 8, input_wantnotto)
-                        for i in range(len(absent_bin_list)):
-                            sheet.update_cell(last_row, 9+i, absent_bin_list[i])
-                    
-                        st.success(f"送信が完了しました。ありがとうございます、{input_name}さん！")
-                    except gspread.exceptions.APIError as e:
-                        st.error("データの送信中にエラーが発生しました。もう一度試してください。")
-                else:
-                    st.warning("必須項目を入力してください。")
+                
+                # ユーザーが送信ボタンを押したときに表示されるメッセージ
+                if submit_button:
+                    if input_name and input_univ and input_level:
+                        # 欠席情報をリストで表現
+                        #absent_bin_list = [1 if option in absent_matches else 0 for option in absent_options]
+                        #while len(absent_bin_list) < 16:
+                        #    absent_bin_list.append(0)
+                        
+                        # スプレッドシートへの書き込み
+                        try:
+                            sheet = gc.open('monketsu-karuta-db').get_worksheet(0)
+                            last_row = len(sheet.col_values(3)) + 1  # 空欄を許すための処置
+                            sheet.append_row([input_taikaiid, input_password, input_name, input_univ, input_level, input_kisuu, input_wantto, input_wantnotto] + absent_bin_list)
+                            
+                            st.success(f"送信が完了しました。ありがとうございます、{input_name}さん！")
+                        except Exception as e:
+                            st.error("データの送信中にエラーが発生しました。もう一度試してください。")
+                    else:
+                        st.warning("必須項目を入力してください。")
         else:
             st.warning("大会名か大会パスワードが間違っています")
 
