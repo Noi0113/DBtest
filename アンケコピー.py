b041a9ef7a6a7578c2ce5ea7c06fb49f947b4fc2
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-16 07:52:28 +0900

    Update アンケコピー.py

--- アンケコピー.py
+++ アンケコピー.py
@@ -11,27 +11,23 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('monketsu-karuta-
 gc = gspread.authorize(credentials)
 
 def main():
-    # 以下は、未だ入力がされていない時に-を表示できるようにした、ということ？？
+    #univ_options = []
+    #absent_options = [] 
     #if 'univ_options' not in st.session_state: 
-     #   st.session_state.univ_options = ["-"]
-    #if 's_number' not in st.session_state: 
-     #   st.session_state.s_number = []
+    #    st.session_state.univ_options = ["-"]
     #if 'absent_options' not in st.session_state: 
-     #   st.session_state.absent_options = ["-"]
-        
-    # まずGoogle Sheetsのシート2を開き、それをデータフレーム化する
+    #    st.session_state.absent_options = ["-"]
+    
     new_gene_sheet = gc.open('monketsu-karuta-db').get_worksheet(1)
     new_gene_data = new_gene_sheet.get_all_values()
     headers = new_gene_data.pop(0)
     new_gene_df = pd.DataFrame(new_gene_data, columns=headers)
     
-    # タイトル
-    st.title('アンケート回答ページコピー') 
+    st.title('アンケート回答ページ！！') 
     st.markdown('参加する大会の大会名とパスワードを入力してください')
     input_taikaiid = st.text_input(label='大会名を入力してください')
     input_password = st.text_input(label="大会パスワードを入力してください", type='password')
     
-    # 新規作成ページで作成された大会IDとパスワードを辞書化
     id_from_df = new_gene_df.iloc[:,0]
     pass_from_df = new_gene_df.iloc[:,1]
     id_list = list(id_from_df)
@@ -39,44 +35,38 @@ def main():
     taikai_dict = dict(zip(id_list, pass_list))
     
     if st.button(label='確定'):
-        # スプシと接続できたからこっちを使う
         if input_taikaiid in taikai_dict and taikai_dict[input_taikaiid] == input_password:
             st.success("{}の参加用フォーム".format(input_taikaiid))
-            # 大会名が入力内容と一致した行を抜き出す必要な情報を取り出す
             filtered_new_gene_df = new_gene_df[new_gene_df.iloc[:,0] == input_taikaiid]
-            # 以下は指定された大会の参加大学数、試合数
             filtered_univ_num = int(filtered_new_gene_df.iloc[0,3])
             filtered_s_num = int(filtered_new_gene_df.iloc[0,2])
-            # 大学の選択肢を作成
+            
             univ_options = [filtered_new_gene_df.iloc[0,4+i] for i in range(filtered_univ_num)]
+            st.session_state.univ_options = univ_options
             
-            # 欠席試合を入力するために、ここで試合のリストを作る
             absent_options = [f'{i+1}試合目' for i in range(filtered_s_num)]
+            st.session_state.absent_options = absent_options
             
-            # フォームを作成します
             with st.form(key='my_form'):
                 input_name = st.text_input(label='名前を入力してください(必須)')
-                input_univ = st.selectbox('学校名または所属会名を入力してください(必須)', options=univ_options)
+                input_univ = st.selectbox('学校名または所属会名を入力してください(必須)', options=st.session_state.univ_options)
                 input_level = st.selectbox('級を入力してください(必須)', options=['A','B','C','D','E'])
                 input_kisuu = st.selectbox('奇数の場合一人取りまたは読手を希望しますか(必ず希望に添えるわけではありません)', options=['はい','いいえ'])
                 input_wantto = st.text_input(label='対戦したい人を記入してください')
                 input_wantnotto = st.text_input(label='対戦したくない人を記入してください')
-                absent_matches = st.multiselect('欠席する試合を入力してください(複数選択可)', options=absent_options)
+                absent_matches = st.multiselect('欠席する試合を入力してください(複数選択可)', options=st.session_state.absent_options)
                 
                 submit_button = st.form_submit_button(label='送信', use_container_width=True)
                 
-                # ユーザーが送信ボタンを押したときに表示されるメッセージ
                 if submit_button:
                     if input_name and input_univ and input_level:
-                        # 欠席情報をリストで表現
-                        #absent_bin_list = [1 if option in absent_matches else 0 for option in absent_options]
-                        #while len(absent_bin_list) < 16:
-                        #    absent_bin_list.append(0)
+                        absent_bin_list = [1 if option in absent_matches else 0 for option in st.session_state.absent_options]
+                        while len(absent_bin_list) < 16:
+                            absent_bin_list.append(0)
                         
-                        # スプレッドシートへの書き込み
                         try:
                             sheet = gc.open('monketsu-karuta-db').get_worksheet(0)
-                            last_row = len(sheet.col_values(3)) + 1  # 空欄を許すための処置
+                            last_row = len(sheet.col_values(3)) + 1
                             sheet.append_row([input_taikaiid, input_password, input_name, input_univ, input_level, input_kisuu, input_wantto, input_wantnotto] + absent_bin_list)
                             
                             st.success(f"送信が完了しました。ありがとうございます、{input_name}さん！")
