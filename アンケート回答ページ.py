5c80c3df89b597f4a9710fee8352f555022f5a83
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-03-01 08:29:03 +0000

    Update アンケート回答ページ.py

--- アンケート回答ページ.py
+++ アンケート回答ページ.py
@@ -33,10 +33,6 @@ def main():
     id_list = list(id_from_df)
     pass_list = list(pass_from_df)
     taikai_dict = dict(zip(id_list,pass_list))
-
-    name_from_df = new_gene_df.iloc[:,2]
-    name_list = list(name_from_df)
-    st.write(name_list)
     
     filtered_univ_num = 0
     filtered_s_num = 0
@@ -62,11 +58,20 @@ def main():
         
         st.success("{}の参加用フォーム".format(input_taikaiid))
 
+        user_sheet = gc.open('monketsu-karuta-db').get_worksheet(0)
+        user_data = user_sheet.get_all_values()
+        user_headers = user_data.pop(0)
+        user_df = pd.DataFrame(user_data, columns = user_headers)
+        filtered_user_df = user_df[user_df.iloc[:,0] == input_taikaiid]
+        st.write(filtered_user_df)
+
         #大会名が入力内容と一致した行を抜き出す必要な情報を取り出す
         filtered_new_gene_df = new_gene_df[new_gene_df.iloc[:,0] == input_taikaiid]
         #以下は指定された大会の参加大学数、試合数
         filtered_univ_num = filtered_new_gene_df.iloc[0,3]
         filtered_s_num = filtered_new_gene_df.iloc[0,2]
+        #st.session_state.name_list = filtered_user_df.iloc[0,1].tolist()
+          
 
         # 大学の選択肢を作成
         st.session_state.univ_options = []
@@ -85,6 +90,8 @@ def main():
             # フォームを作成します
     with st.form(key='my_form'):
             input_name = st.text_input(label='名前を入力してください(必須)')
+            #if input_name in name_list:
+            #st.warning('同じ名前の人がいるため、記載内容を上書きします。別人の場合は名前を変えてください。'）
             input_univ = st.selectbox('学校名または所属会名を入力してください(必須)', options=st.session_state.univ_options)
             input_level = st.selectbox('級を入力してください(必須)',options=['A','B','C','D','E'])
             input_kisuu = st.selectbox('奇数の場合一人取りまたは読手を希望しますか(必ず希望に添えるわけではありません)',options=['はい','いいえ'])
@@ -97,9 +104,8 @@ def main():
                 # ユーザーが送信ボタンを押したとき
             if submit_button:
                 if input_name and input_univ and input_level:
-                    #if input_name in
                     try:
-                        sheet = gc.open('monketsu-karuta-db').get_worksheet(0)
+                        
                         last_row = len(sheet.col_values(3)) + 1
                         absent_bin_list = [1 if option in absent_matches else 0 for option in st.session_state.absent_options]
                         sheet.append_row([input_taikaiid, input_password, input_name, input_univ, input_level, input_kisuu, input_wantto, input_wantnotto] + absent_bin_list)
