9fd4f6613c3eefc2c59329ee943606524075d8fe
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-03-01 19:13:44 +0900

    Update アンケート回答ページ.py

--- アンケート回答ページ.py
+++ アンケート回答ページ.py
@@ -56,6 +56,8 @@ def main():
         user_headers = user_data.pop(0)
         user_df = pd.DataFrame(user_data, columns = user_headers)
         filtered_user_df = user_df[user_df.iloc[:,0] == input_taikaiid]
+        
+          
         #大会名が入力内容と一致した行を抜き出す必要な情報を取り出す
         filtered_new_gene_df = new_gene_df[new_gene_df.iloc[:,0] == input_taikaiid]
         #以下は指定された大会の参加大学数、試合数
@@ -63,7 +65,11 @@ def main():
         filtered_s_num = filtered_new_gene_df.iloc[0,2]
         st.session_state.name_list = filtered_user_df['名前'].tolist()
         st.write(st.session_state.name_list)
-
+        if "田中" in st.session_state.name_list:
+            matching_rows = user_sheet.findall("田中", in_column="名前")
+            st.write(matching_rows)
+          
+          
         # 大学の選択肢を作成
         st.session_state.univ_options = []
         for i in range(int(filtered_univ_num)):
