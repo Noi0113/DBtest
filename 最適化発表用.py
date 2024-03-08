0850d6de42d95e5b9ed5abe69d1f4687e84ff3bd
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-03-08 14:27:43 +0900

    Update 最適化発表用.py

--- 最適化発表用.py
+++ 最適化発表用.py
@@ -1,19 +1,29 @@
 import streamlit as st
 import time
 import pandas as pd
+def main():
+    status_area = st.empty()
+    #ここから上は編集しない
 
-# CSVファイルのパス
-csv_file_path = "data25結果.csv"  # CSVファイルのパスを適切なものに置き換える
+    #タイトル
+    st.title('対戦表の作成')
+    #install coin-or-cbc
 
-# ボタンが押されたときの処理
-if st.button("CSVファイルを読み込む"):
-    # ボタンが押されたら10秒待つ
-    with st.spinner("対戦表を作成中..."):
-        time.sleep(10)
-    
-    # CSVファイルを読み込む
-    try:
-        df = pd.read_csv(csv_file_path)
-        st.dataframe(df)
-    except FileNotFoundError:
-        st.error("CSVファイルが見つかりませんでした。")
+    st.markdown('対戦表を作成したい大会の大会名・大会パスワードを入力してください')
+    input_taikaiid = st.text_input(label = '大会名')
+    input_password = st.text_input(label = 'パスワード',type = 'password')
+    if st.button('対戦表の作成',use_container_width=True):
+        st.success("対戦表を作成します")
+        with st.spinner("対戦表を作成中..."):
+            time.sleep(10)
+        
+        # CSVファイルを読み込む
+        try:
+            df = pd.read_csv(csv_file_path)
+            st.dataframe(df)
+        except FileNotFoundError:
+            st.error("CSVファイルが見つかりませんでした。")
+
+ #   hashed_pswd = make_hashes(input_password)
+ #   result = login_user(input_taikaiid,check_hashes(input_password,hashed_pswd))
+ #   if result:
