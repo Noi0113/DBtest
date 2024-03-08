e8824dd5b077588c52ee8459bc47cad83b4ae917
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-03-08 14:40:25 +0900

    Update 最適化発表用.py

--- 最適化発表用.py
+++ 最適化発表用.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import time
 import pandas as pd
+
 def main():
     #status_area = st.empty()
     #ここから上は編集しない
@@ -10,22 +11,24 @@ def main():
     #install coin-or-cbc
 
     st.markdown('対戦表を作成したい大会の大会名・大会パスワードを入力してください')
-    input_taikaiid = st.text_input(label = '大会名')
-    input_password = st.text_input(label = 'パスワード',type = 'password')
-    if st.button('対戦表の作成',use_container_width=True):
+    input_taikaiid = st.text_input(label='大会名')
+    input_password = st.text_input(label='パスワード', type='password')
+    if st.button('対戦表の作成', use_container_width=True):
         st.success("対戦表を作成します")
         with st.spinner("対戦表を作成中..."):
             time.sleep(10)
         
         # CSVファイルを読み込む
+        csv_file_path = "data25結果.csv"  # CSVファイルのパスを適切なものに置き換える
         try:
-            df = pd.read_csv(data25結果.csv)
+            df = pd.read_csv(csv_file_path)
             st.dataframe(df)
         except FileNotFoundError:
             st.error("CSVファイルが見つかりませんでした。")
 
- #   hashed_pswd = make_hashes(input_password)
- #   result = login_user(input_taikaiid,check_hashes(input_password,hashed_pswd))
- #   if result:
+    #   hashed_pswd = make_hashes(input_password)
+    #   result = login_user(input_taikaiid,check_hashes(input_password,hashed_pswd))
+    #   if result:
+
 if __name__ == '__main__':
     main()
